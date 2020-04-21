e289b4cc44d05b61d4a9fdca7d3d22acb87a8ef0
Salvador Guzman Jr <guzmansalv@gmail.com>
2020-04-21 08:11:40 -0700

    stub epub api call

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const epub = require('epub');
+const epubAPI = require('epub');
 const cheerio = require('cheerio');
+
+const filePath = 'The Cathedral and the Bazaar - Eric S. Raymond.epub'
+const epub = new epubAPI(filePath);
+
+epub.on("end", function(){
+    // epub is now usable
+    epub.flow.forEach(function(chapter){
+        console.log(chapter);
+        epub.getChapter(chapter.id, function(err, text){
+            console.log(text);
+        });
+    });
+
+
+});
+
+epub.parse();
