f93108cfcfaffdd6fec53e446a91bf21332314ba
Salvador Guzman Jr <guzmansalv@gmail.com>
2020-04-25 18:57:35 -0700

    working extraction api stuff

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const epubAPI = require('epub');
 const cheerio = require('cheerio');
 
-const filePath = 'The Cathedral and the Bazaar - Eric S. Raymond.epub'
+const filePath = 'The Cathedral and the Bazaar - Eric S. Raymond.epub';
 const epub = new epubAPI(filePath);
 
 epub.on("end", function(){
     // epub is now usable
+    console.log(epub.metadata);
     epub.flow.forEach(function(chapter){
         console.log(chapter);
-        epub.getChapter(chapter.id, function(err, text){
-            console.log(text);
-        });
     });
 
 
